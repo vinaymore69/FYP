834c1fa8ecf9bdda46c92167af735082d417a8c6
Vinay More <129585051+vinaymore69@users.noreply.github.com>
2025-01-06 14:49:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+
+
+function page4Animation(){
+    
+var elemC= document.querySelector('#elem-container');
+var fixed=document.querySelector("#fixed-image");
+elemC.addEventListener("mouseenter", function(){
+    fixed.style.display="block";
+
+});
+elemC.addEventListener("mouseleave", function(){
+    fixed.style.display="none";
+
+});
+
+var elems = document.querySelectorAll(".elem");
+elems.forEach(function (e) {
+    e.addEventListener("mouseenter", function () {
+        var image = e.getAttribute("data-image");
+        fixed.style.backgroundImage = `url(${image})`;
+    });
+});
+}
+
+
+page4Animation();
+
+
